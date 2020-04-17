cbd3f000a21baee0633c97cb904458791f5d931a
Easton Potokar <contagon6@gmail.com>
2020-04-16 20:56:41 -0600

    Got hdf5 and eigen hooked in

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Summer Project to freshen up on my C++ skills. Seeking to implement various sklearn algorithms in C++.
+
+## Dependencies
+  * [Eigen](https://eigen.tuxfamily.org/dox/group__TutorialMatrixArithmetic.html): C++ Numpy Equivalent. [Installation](https://eigen.tuxfamily.org/dox/GettingStarted.html) (or available via apt)
+  * [fdcl-hdf5](https://github.com/skulumani/fdcl-hdf5): Used to save eigen data to HD5 files. [Installation](https://shankarkulumani.com/2018/09/hdf5.html).
+
+## Resources
+  * [C++ Tutorials](https://github.com/mhask94/cpp_tutorials). I've been following these to get used to cmake and eigen. Made by Matt Haskell here at BYU.
---
+++ main.cpp
@@ -0,0 +1,54 @@
+//Header file for input output functions 
+#include "hdf5.hpp"
+#include <Eigen/Dense>
+#include <iostream>  
+
+using namespace std; 
+
+void write_data() {
+    Eigen::MatrixXd matrix(3, 3);
+    matrix << 1, 2, 3, 4, 5, 6, 7, 8, 11;
+
+    // open the file 
+    HDF5::File hf = HDF5::File("filename.hdf5", HDF5::File::Truncate);
+
+    // write the data
+    hf.write("dataset_name", matrix);
+    
+    std::cout << "Original Matrix: " << std::endl;
+    std::cout << matrix << std::endl;
+}
+
+void read_data() {
+    // open the file for reading
+    HDF5::File hf = HDF5::File("filename.hdf5", HDF5::File::ReadOnly);
+
+    Eigen::MatrixXd matrix;
+
+    hf.read("dataset_name", matrix);
+    
+    std::cout << "Matrix read: " << std::endl;
+    std::cout << matrix << std::endl;
+}
+
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+	// prints hello world 
+	cout << "Hello World" << endl; 
+	cout << "It's all starting to come back!" << endl; 
+
+	Eigen::Matrix<double,3,3> m;
+	m << 5,0,0, 0,1,0, 0,0,1;
+
+	Eigen::Vector3d v;
+	v << 1,-2,3;
+	cout << m*v << endl;
+	// cout << m.size() << endl;
+
+	write_data();
+	read_data();
+
+	return 0; 
+} 
